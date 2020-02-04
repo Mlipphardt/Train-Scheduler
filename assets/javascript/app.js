a1cc67e5e461ac5d1cc20b6129ced57257378ac7
Mlipphardt <matthewlipphardt@gmail.com>
2020-02-04 17:57:35 -0500

    Now uses regular expressions to halt invalid user input and alert them.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,16 +15,21 @@ var firebaseConfig = {
 
   let db = firebase.database();
 
-//   On-click event for button to add new train
-  $("#add-train-btn").on("click", function(event) {
-    event.preventDefault();
+  //Regular expression to enforce proper time format on user input
+  let acceptableFormat = /\d\d:\d\d/;
   
+//   On-click event for button to add new train
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+  var trainStart = $("#start-input").val().trim();
+  //Only stores values in firebase if user conforms to "HH:mm" format.
+  if (acceptableFormat.test(trainStart)){
     // Grabs user input
     var trainName = $("#train-name-input").val().trim();
     var trainDest = $("#destination-input").val().trim();
-    var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("X");
+    trainStart = moment($("#start-input").val().trim(), "HH:mm").format("X");
     var trainFreq = $("#frequency-input").val().trim();
-  
+
     // Creates local "temporary" object for holding employee data
     var newTrain = {
       name: trainName,
@@ -32,7 +37,7 @@ var firebaseConfig = {
       start: trainStart,
       frequency: trainFreq
     };
-  
+
     // Uploads employee data to the database
     db.ref().push(newTrain);
 
@@ -41,41 +46,45 @@ var firebaseConfig = {
     $("#destination-input").val("");
     $("#start-input").val("");
     $("#frequency-input").val("");
-})
+    } else {
+      alert("Please use the time format 'HH: mm'!")
+    };
+  });
 
 db.ref().on("child_added", function(childSnapshot) {
 
-    var trainName = childSnapshot.val().name;
-    var trainDest = childSnapshot.val().destination;
-    var trainStart = childSnapshot.val().start;
-    var trainFreq = childSnapshot.val().frequency;
+var trainName = childSnapshot.val().name;
+var trainDest = childSnapshot.val().destination;
+var trainStart = childSnapshot.val().start;
+var trainFreq = childSnapshot.val().frequency;
 
-    var trainStartdeUnixer = moment.unix(trainStart).format("MM/DD/YYYY");
+var trainStartdeUnixer = moment.unix(trainStart).format("MM/DD/YYYY");
 
-    var trainStartConverted = moment(trainStartdeUnixer, "HH:mm").subtract(1, "years");
+var trainStartConverted = moment(trainStartdeUnixer, "HH:mm").subtract(1, "years");
 
-    var diffTime = moment().diff(moment(trainStartConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-    
-    var tRemainder = diffTime % trainFreq;
-    console.log(tRemainder);
-    
-    var tMinutesTillTrain = trainFreq - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-    
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-    
-    
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDest),
-        $("<td>").text(trainFreq),
-        $("<td>").text(moment(nextTrain).format("hh:mm")),
-        $("<td>").text(tMinutesTillTrain)
-      );
-    
-      // Append the new row to the table
-      $("#train-table > tbody").append(newRow);
+var diffTime = moment().diff(moment(trainStartConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
+
+var tRemainder = diffTime % trainFreq;
+console.log(tRemainder);
+
+var tMinutesTillTrain = trainFreq - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDest),
+    $("<td>").text(trainFreq),
+    $("<td>").text(moment(nextTrain).format("hh:mm")),
+    $("<td>").text(tMinutesTillTrain)
+  );
+
+  // Append the new row to the table
+  $("#train-table > tbody").append(newRow);
 
 });
+
